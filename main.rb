69592fc0befb86c4eaf5ee5b899da1cd134bd9fc
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-09-02 14:24:37 +0100

    Refactor main.rb to continuously run tests until they pass

--- main.rb
+++ main.rb
@@ -6,28 +6,34 @@ course = Store.instance.models_for(Course).detect { |course| course.slug == "bit
 stages = Store.instance.models_for(CourseStage).select { |stage| stage.course_id == course.id }
 first_stage = stages.min_by(&:position)
 
-current_code = File.read("current_repository/app/main.py")
-test_runner_output = TestRunner.new("current_repository").run_tests
+counter = 0
 
-result = TestPrompt.call(
-  stage: first_stage,
-  course: course,
-  original_code: current_code,
-  test_runner_output: test_runner_output
-).result
+loop do
+  current_code = File.read("current_repository/app/main.py")
+  test_runner_output = TestRunner.new("current_repository").run_tests
 
-puts result
-edited_code = result.scan(/```python\n(.*?)```/m).join("\n")
+  break if test_runner_output.passed?
 
-puts "Diff:"
-puts ""
-puts ""
+  counter += 1
+  puts "Attempt #{counter}!"
 
-Diffy::Diff.default_format = :color
-puts Diffy::Diff.new(original_code, extracted_code, context: 2)
+  puts test_runner_output.last_stage_logs
 
-File.write("current_repository/app/main.py", edited_code)
-test_runner_output = TestRunner.new("current_repository").run_tests
+  result = TestPrompt.call(
+    stage: first_stage,
+    course: course,
+    original_code: current_code,
+    test_runner_output: test_runner_output
+  ).result
 
-puts "Passed: #{test_runner_output.passed?}"
-puts test_runner_output.last_stage_logs
+  edited_code = result.scan(/```python\n(.*?)```/m).join("\n")
+
+  puts "Diff:"
+  puts ""
+  puts ""
+
+  Diffy::Diff.default_format = :color
+  puts Diffy::Diff.new(current_code, edited_code, context: 2)
+
+  File.write("current_repository/app/main.py", edited_code)
+end
