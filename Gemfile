1888a617d57bf71be722b6b48af2b166d266c6f0
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-09-02 12:16:52 +0100

    Add zeitwerk gem and new lib/test.rb file

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source "https://rubygems.org"
 
 gem "activesupport"
 gem "ruby-openai"
+gem "zeitwerk"
---
+++ main.rb
@@ -0,0 +1,9 @@
+require 'bundler'
+Bundler.require
+
+# Autoload all files in ./lib
+loader = Zeitwerk::Loader.new
+loader.push_dir("#{__dir__}/lib")
+loader.setup
+
+puts "Hello world! #{Test.new}"
