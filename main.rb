149ce321408ddfde8aba10d19b06652d829c15a7
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-09-02 13:46:54 +0100

    Update Gemfile and Gemfile.lock to include the "diffy" gem. Modify main.rb to include the original code provided. Run the TestPrompt with the updated code. Extract the code from the result and display the diff between the original code and the extracted code.

--- main.rb
+++ main.rb
@@ -6,4 +6,61 @@ course = Store.instance.models_for(Course).detect { |course| course.slug == "bit
 stages = Store.instance.models_for(CourseStage).select { |stage| stage.course_id == course.id }
 first_stage = stages.min_by(&:position)
 
-puts TestPrompt.call(stage: first_stage, course: course).result
+original_code = <<~CODE
+  import json
+  import sys
+
+  # import bencodepy - available if you need it!
+  # import requests - available if you need it!
+
+  # Examples:
+  #
+  # - decode_bencode(b"5:hello") -> b"hello"
+  # - decode_bencode(b"10:hello12345") -> b"hello12345"
+  def decode_bencode(bencoded_value):
+      if chr(bencoded_value[0]).isdigit():
+          length = int(bencoded_value.split(b":")[0])
+          return bencoded_value.split(b":")[1][:length]
+      else:
+          raise NotImplementedError("Only strings are supported at the moment")
+
+
+  def main():
+      command = sys.argv[1]
+
+      # You can use print statements as follows for debugging, they'll be visible when running tests.
+      print("Logs from your program will appear here!")
+
+      if command == "decode":
+          bencoded_value = sys.argv[2].encode()
+
+          # json.dumps() can't handle bytes, but bencoded "strings" need to be
+          # bytestrings since they might contain non utf-8 characters.
+          #
+          # Let's convert them to strings for printing to the console.
+          def bytes_to_str(data):
+              if isinstance(data, bytes):
+                  return data.decode()
+
+              raise TypeError(f"Type not serializable: {type(data)}")
+
+          # Uncomment this block to pass the first stage
+          # print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
+      else:
+          raise NotImplementedError(f"Unknown command {command}")
+
+
+  if __name__ == "__main__":
+      main()
+CODE
+
+result = TestPrompt.call(stage: first_stage, course: course, original_code: original_code).result
+puts result
+extracted_code = result.scan(/```python\n(.*?)```/m).join("\n")
+
+puts "Diff:"
+puts ""
+puts ""
+
+Diffy::Diff.default_format = :color
+puts Diffy::Diff.new(original_code, extracted_code, context: 2)
--- prompts/test_prompt.rb
+++ prompts/test_prompt.rb
@@ -3,6 +3,7 @@ class TestPrompt
 
   def call
     client = OpenAI::Client.new(access_token: ENV["OPENAI_API_KEY"], request_timeout: 240)
+    puts system_message
 
     raw_response = client.chat(
       parameters: {
@@ -44,51 +45,7 @@ class TestPrompt
       The user is asking you to help them edit their code to pass this stage. The user's code is listed below delimited by triple backticks:
 
       ```python
-      import json
-      import sys
-
-      # import bencodepy - available if you need it!
-      # import requests - available if you need it!
-
-      # Examples:
-      #
-      # - decode_bencode(b"5:hello") -> b"hello"
-      # - decode_bencode(b"10:hello12345") -> b"hello12345"
-      def decode_bencode(bencoded_value):
-          if chr(bencoded_value[0]).isdigit():
-              length = int(bencoded_value.split(b":")[0])
-              return bencoded_value.split(b":")[1][:length]
-          else:
-              raise NotImplementedError("Only strings are supported at the moment")
-
-
-      def main():
-          command = sys.argv[1]
-
-          # You can use print statements as follows for debugging, they'll be visible when running tests.
-          print("Logs from your program will appear here!")
-
-          if command == "decode":
-              bencoded_value = sys.argv[2].encode()
-
-              # json.dumps() can't handle bytes, but bencoded "strings" need to be
-              # bytestrings since they might contain non utf-8 characters.
-              #
-              # Let's convert them to strings for printing to the console.
-              def bytes_to_str(data):
-                  if isinstance(data, bytes):
-                      return data.decode()
-
-                  raise TypeError(f"Type not serializable: {type(data)}")
-
-              # Uncomment this block to pass the first stage
-              # print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
-          else:
-              raise NotImplementedError(f"Unknown command {command}")
-
-
-      if __name__ == "__main__":
-          main()
+      #{context.original_code}
       ```
 
       Fix the user's code so that it passes the stage.
