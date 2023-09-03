198cb7f674bf3a78cf0af889c4368aae7bd9a748
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-09-03 13:53:09 +0100

    Add cache miss logging in BasePrompt class

--- prompts/base_prompt.rb
+++ prompts/base_prompt.rb
@@ -10,6 +10,8 @@ class BasePrompt
     signature_hash = Digest::SHA256.hexdigest(signature)
 
     self.class.diskcache.cache(signature_hash) do
+      puts "Cache miss, calling OpenAI API with signature #{signature_hash}"
+
       client = OpenAI::Client.new(access_token: ENV["OPENAI_API_KEY"], request_timeout: 240)
       client.chat(*args, **kwargs)
     end
