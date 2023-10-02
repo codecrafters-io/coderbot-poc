e38804c084c84f3eae26a0625a5e646c74f70f3e
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-10-03 00:09:24 +0100

    more updates

--- lib/test_runner_output.rb
+++ lib/test_runner_output.rb
@@ -22,6 +22,11 @@ class TestRunnerOutput
     blocks.reverse.find { |block| block.gsub(/\e\[(\d+)m/, "").start_with?("[stage-") }
   end
 
+  def failed_stage
+    stage_position = last_stage_logs_without_colors.match(/^\[stage-(\d+)\]/)[1].to_i
+    Store.instance.models_for(CourseStage).find { |stage| stage.course_id == course.id && stage.position == stage_position }
+  end
+
   def last_stage_logs_without_colors
     last_stage_logs.gsub(/\e\[(\d+)m/, "")
   end
